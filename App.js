3316796de34e65fae6dc476297d355b534e77ede
John  Tucker <john@larkintuckerllc.com>
2018-10-06 06:49:00 -0400

    Ready

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello - Open up App.js to start working on your app!</Text>
+        <Text>Constants.expoVersion</Text>
       </View>
     );
   }
