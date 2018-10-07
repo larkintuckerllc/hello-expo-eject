9a48fe326b7215f0ca10a39c52f64c2ddc0c03f6
John Tucker <john@larkintuckerllc.com>
2018-10-07 00:11:40 -0400

    Ejected

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp2395969fd47b438e83262690fa43d37b://192.168.1.38:19000";
+
+}
